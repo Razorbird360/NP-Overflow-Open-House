f13fa686583888ff1eeea48308a725e53b467d87
Ranen <ranensim.yangyi@gmail.com>
2024-12-15 19:49:06 +0800

    leonard is here

---
+++ main.js
@@ -0,0 +1,50 @@
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+
+
+const scene = new THREE.Scene();
+
+const renderer = new THREE.WebGLRenderer({
+  canvas: document.querySelector('#canvas'),
+});
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.set(0, 20, 0);
+
+const controls = new OrbitControls(camera, renderer.domElement);
+
+const axesHelper = new THREE.AxesHelper(5);
+scene.add(axesHelper);
+
+const planeGeometry = new THREE.PlaneGeometry(30, 30);
+const planeMaterial = new THREE.MeshBasicMaterial({ color: 0xFFFFFF, side: THREE.DoubleSide });
+const plane = new THREE.Mesh(planeGeometry, planeMaterial);
+plane.rotation.x = Math.PI / 2;
+scene.add(plane);
+
+const ambientLight = new THREE.AmbientLight(0xFFFFFF, 1);
+scene.add(ambientLight);
+
+
+//leonard GLTF
+const loader = new GLTFLoader();
+loader.load(
+  '/resources/leonard/leonard_animated.glb', 
+  function (gltf) {
+    const guy = gltf.scene;
+    scene.add(guy);
+    gltf.scene.scale.set(1, 1, 1);
+    gltf.scene.position.set(0, 0, 0);
+  }
+);
+
+camera.position.z = 5;
+
+function animate() {
+  requestAnimationFrame(animate);
+  renderer.render(scene, camera);
+}
+renderer.setAnimationLoop(animate);
