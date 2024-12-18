14b3cdb772795fd138c2fcb0b996c4e7a1f2392d
Raymond Goo <raymond.goo124@gmail.com>
2024-12-18 19:05:19 +0900

    chore: include vite as dependency

--- main.js
+++ main.js
@@ -8,6 +8,7 @@ const scene = new THREE.Scene();
 const renderer = new THREE.WebGLRenderer({
   canvas: document.querySelector('#canvas'),
 });
+
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
