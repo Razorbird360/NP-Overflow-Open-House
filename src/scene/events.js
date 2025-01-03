860af7bae00a084f26903874dafcfccf11bd8a59
Alluru Rishitha Reddy <167403788+Rishitha-alluru@users.noreply.github.com>
2025-01-04 01:41:52 +0800

    Removed description
    description is added in the pop up instead

--- src/scene/events.js
+++ src/scene/events.js
@@ -33,30 +33,6 @@ export const loadEvents = async (scene) => {
       text.position.set(initialPosition.x - 1, initialPosition.y + 2, initialPosition.z + (-5 * i));
       scene.add(text);
 
-      const maxLineLength = 30;
-      const words = event.description.split(' ');
-      const lines = words.reduce((acc, word) => {
-        if (acc[acc.length - 1].length + word.length + 1 <= maxLineLength) {
-          acc[acc.length - 1] += ' ' + word;
-        } else {
-          acc.push(word);
-        }
-        return acc;
-      }, ['']);
-
-      lines.forEach((line, index) => {
-        const lineGeometry = new TextGeometry(line, {
-          font: font,
-          size: 0.15,
-          depth: 0.01,
-        });
-        const lineMaterial = new MeshBasicMaterial({ color: 0xAAAAAA });
-        const lineText = new Mesh(lineGeometry, lineMaterial);
-        lineText.position.set(initialPosition.x - 1, initialPosition.y - 1 - index * 0.2, initialPosition.z + (-5 * i));
-        scene.add(lineText);
-      });
-    });
-
     const texture = await new TextureLoader().loadAsync(`${BASE_PATH}images/${event.image}`);
     texture.colorSpace = SRGBColorSpace;
     texture.generateMipmaps = true;
