018d0c8bae490207dc9006bc8a69d3a5e3ca4b58
Ranen <ranensim.yangyi@gmail.com>
2025-01-06 22:40:32 +0800

    Removed axeshelper

--- src/scene/scene.js
+++ src/scene/scene.js
@@ -30,7 +30,7 @@ export function createScene() {
 
   // Add helper axes
   const axesHelper = new THREE.AxesHelper(5);
-  scene.add(axesHelper);
+  // scene.add(axesHelper);
 
   // Add ambient light
   const ambientLight = new THREE.AmbientLight(0xffffff, 1);
